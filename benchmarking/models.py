f6df0a96e736014dae2805af9152c23eccceb120
karl-richter <karl.richter@tum.de>
2023-03-20 22:42:19 -0700

    updated benchmarking

--- benchmarking/models.py
+++ benchmarking/models.py
@@ -3,33 +3,54 @@ import numpy as np
 import matplotlib.pyplot as plt
 from benchmarking.utils import split_data, naive_forecasts, calculate_metrics
 
-from modularprophet.compositions import Additive
-from modularprophet.components import Trend
-from modularprophet.modules.seasonality import FourierSeasonality
 from modularprophet.forecaster import ModularProphet
 
 
+def calculate_component_attributions(components, y, n_lags):
+    components = {k: v for k, v in components.items() if k != "y_hat"}
+    r2 = {}
+    y = y[n_lags:]
+    cache = np.zeros_like(y, dtype=np.float32)
+    for name, component in components.items():
+        # extract 1-step ahead forecasts
+        cache += np.concatenate(
+            [
+                np.array([c[0] for c in component], dtype=np.float32),
+                np.array(component[-1][1:], dtype=np.float32),
+            ],
+            dtype=np.float32,
+        )
+        # R2 Score
+        numerator = ((y - cache) ** 2).sum(axis=0, dtype=np.float32)
+        denominator = ((y - np.average(y, axis=0)) ** 2).sum(axis=0, dtype=np.float32)
+        r2[name] = 1 - (numerator / denominator)
+    return r2
+
+
 def evaluate_modularprophet(
-    dataset, n_forecasts, n_lags, step_size, holdout, seasonal_cycle, config={}
+    dataset,
+    n_forecasts,
+    n_lags,
+    step_size,
+    holdout,
+    seasonal_cycle,
+    model=None,
+    constraint=None,
 ):
     df = pd.read_csv(dataset)
     num_splits = (holdout - 2 * n_forecasts) + 1
     metrics = {}
     predictions = []
+    attributions = {}
 
     for i in range(0, num_splits, step_size):
         train_df, eval_df, y = split_data(df, i, holdout, n_forecasts, n_lags)
-        forecaster = ModularProphet(
-            Additive(
-                Trend(),
-                FourierSeasonality(
-                    "yearly", period=365.25, series_order=3
-                ),  # multiply_with="Trend"
-            )
+        forecaster = ModularProphet(model)
+        _ = forecaster.fit(
+            train_df, n_forecasts=n_forecasts, learning_rate=0.1, constraint=constraint
         )
-        _ = forecaster.fit(train_df, n_forecasts=n_forecasts, learning_rate=0.1)
         # Evaluation
-        _, y, y_hat, _ = forecaster.predict(eval_df)
+        _, _, y_hat, _ = forecaster.predict(eval_df)
         prediction = y_hat.squeeze(0)
         predictions.append(
             np.pad(
@@ -42,4 +63,12 @@ def evaluate_modularprophet(
         # Metrics
         naive, seasonal_naive = naive_forecasts(train_df, seasonal_cycle, n_forecasts)
         metrics[i] = calculate_metrics(y, prediction, naive, seasonal_naive)
-    return pd.DataFrame(metrics).T, pd.DataFrame(np.array(predictions)).T
+        # Calculate component attributions
+        _, _, _, components = forecaster.predict(train_df)
+        y = train_df["y"].values
+        attributions[i] = calculate_component_attributions(components, y, n_lags)
+    return (
+        pd.DataFrame(metrics).T,
+        pd.DataFrame(np.array(predictions)).T,
+        pd.DataFrame(attributions).T,
+    )
--- benchmarking/utils.py
+++ benchmarking/utils.py
@@ -5,8 +5,16 @@ import pandas as pd
 def calculate_metrics(y, y_hat, naive, seasonal_naive):
     mae = 1 / len(y) * np.sum(np.abs(y - y_hat))
     rmse = np.sqrt(1 / len(y) * np.sum((y - y_hat) ** 2))
-    mase = 1 / len(y) * np.sum(np.abs(y - y_hat) / (1 / len(y) * np.sum(np.abs(y - naive))))
-    smase = 1 / len(y) * np.sum(np.abs(y - y_hat) / (1 / len(y) * np.sum(np.abs(y - seasonal_naive))))
+    mase = (
+        1
+        / len(y)
+        * np.sum(np.abs(y - y_hat) / (1 / len(y) * np.sum(np.abs(y - naive))))
+    )
+    smase = (
+        1
+        / len(y)
+        * np.sum(np.abs(y - y_hat) / (1 / len(y) * np.sum(np.abs(y - seasonal_naive))))
+    )
     mape = 1 / len(y) * np.sum(np.abs(y - y_hat) / y)
     return {"MAE": mae, "RMSE": rmse, "MAPE": mape, "MASE": mase, "sMASE": smase}
 
@@ -15,9 +23,9 @@ def naive_forecasts(train_df, seasonal_cycle, n_forecasts):
     # Extract the last value from the training set and repeat it n_forecasts times
     naive = train_df["y"].tail(1).values * np.ones(n_forecasts)
     # Extract the last seasonal_cycle values from the training set and repeat them n_forecasts // seasonal_cycle + 1 times
-    seasonal_naive = np.tile(train_df["y"].tail(seasonal_cycle).values, n_forecasts // seasonal_cycle + 1)[
-        :n_forecasts
-    ]
+    seasonal_naive = np.tile(
+        train_df["y"].tail(seasonal_cycle).values, n_forecasts // seasonal_cycle + 1
+    )[:n_forecasts]
     return naive, seasonal_naive
 
 
@@ -28,16 +36,12 @@ def split_data(df, i, holdout, n_forecasts, n_lags):
     return train_df, eval_df, y
 
 
-def store_dataframes(metrics, predictions, dataset, model_name):
+def store_dataframes(metrics, predictions, components, dataset, model_name):
     dataset = dataset.split("/")[-1].split(".")[0]
-    metrics.to_csv(f"{dataset}/{model_name}_metrics.csv", index=False)
-    predictions.to_csv(f"{dataset}/{model_name}_predictions.csv", index=False)
-
-
-def calculate_component_attributions(components, y, naive, n_forecasts):
-    cache = np.zeros(n_forecasts)
-    baseline = naive
-    for component in components.columns:
-        cache += forecast[component].values
-        mase = 1 - (1 / len(y) * np.sum(np.abs(y - cache) / (1 / len(y) * np.sum(np.abs(y - baseline)))))
-        print(component, mase)
+    metrics.to_csv(f"benchmarking/{dataset}/{model_name}_metrics.csv", index=False)
+    predictions.to_csv(
+        f"benchmarking/{dataset}/{model_name}_predictions.csv", index=False
+    )
+    components.to_csv(
+        f"benchmarking/{dataset}/{model_name}_components.csv", index=False
+    )
